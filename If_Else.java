fbc3a0120c3f88c5a0222289205ca28e8e90c567
AabhaAB <133474263+AabhaAB@users.noreply.github.com>
2024-02-05 23:45:26 +0530

    Update

--- If_Else.java
+++ If_Else.java
@@ -5,7 +5,7 @@ public class If_Else {
 	public static void main(String[] args) {
 		// 
 		int a=10, b=20;
-		if (a>b)
+		if (a<b)
 			System.out.println("a is less than b");
 		else
 			System.out.println("a is greater than b");	
